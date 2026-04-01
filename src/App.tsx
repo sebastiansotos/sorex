e4c109b9665ad0438828e387a23cf27c2fb3befc
Sebastiansotos <sebastiansotogamerxd@gmail.com>
2026-04-01 15:49:53 -0400

    Description Change

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ function App() {
         <div style={{ textAlign: 'center', padding: '0 20px' }}>
           <h1 className="titulo-glitch">𝘏𝘦𝘭𝘭𝘰, 𝘞𝘦𝘭𝘤𝘰𝘮𝘦 𝘵𝘰 𝘚𝘰𝘳𝘦𝘹</h1>
           <p className="descripcion-hero">
-             𝘋𝘦𝘴𝘪𝘨𝘯𝘴 𝘧𝘰𝘳 𝘵𝘩𝘰𝘴𝘦 𝘸𝘩𝘰 𝘥𝘰 𝘯𝘰𝘵 𝘴𝘦𝘦𝘬 𝘵𝘰 𝘦𝘯𝘤𝘢𝘮𝘱, 𝘪𝘧 𝘯𝘰𝘵 𝘵𝘰 𝘥𝘰𝘮𝘪𝘯𝘢𝘵𝘦. 𝘐𝘧 𝘺𝘰𝘶'𝘳𝘦 𝘯𝘰𝘵 𝘸𝘪𝘭𝘭𝘪𝘯𝘨 𝘵𝘰 𝘳𝘪𝘴𝘬 𝘦𝘷𝘦𝘳𝘺𝘵𝘩𝘪𝘯𝘨, 𝘥𝘰𝘯'𝘵 𝘦𝘷𝘦𝘯 𝘵𝘳𝘺.
+             𝘋𝘦𝘴𝘪𝘨𝘯𝘦𝘥 𝘧𝘰𝘳 𝘵𝘩𝘰𝘴𝘦 𝘸𝘩𝘰 𝘥𝘰𝘯'𝘵 𝘴𝘦𝘦𝘬 𝘵𝘰 𝘧𝘪𝘵 𝘪𝘯, 𝘪𝘧 𝘯𝘰𝘵 𝘵𝘰 𝘥𝘰𝘮𝘪𝘯𝘢𝘵𝘦. 𝘐𝘧 𝘺𝘰𝘶'𝘳𝘦 𝘯𝘰𝘵 𝘸𝘪𝘭𝘭𝘪𝘯𝘨 𝘵𝘰 𝘳𝘪𝘴𝘬 𝘦𝘷𝘦𝘳𝘺𝘵𝘩𝘪𝘯𝘨, 𝘥𝘰𝘯'𝘵 𝘦𝘷𝘦𝘯 𝘵𝘳𝘺.
              <br />
              <strong style={{ color: 'white', display: 'block', marginTop: '10px' }}>𝘌𝘯𝘨𝘪𝘯𝘦𝘦𝘳𝘦𝘥 𝘧𝘰𝘳 𝘵𝘩𝘦 𝘦𝘭𝘪𝘵𝘦.</strong>
           </p>
